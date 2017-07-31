85248da77d6f6ffc90e88fab2d4f73761f091216
Kapital Bank <developer@Pashas-MacBook-Air.local>
2017-07-31 16:56:10 +0400

    Geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,6 @@
 var app = {
     options: {
-        filter: 6
+        filter: 3
     },
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
@@ -9,12 +9,31 @@ var app = {
         this.receivedEvent('deviceready');
     },
     receivedEvent: function(id) {
-        var watcher = navigator.compass.watchHeading(this.onCompassSucess, this.onCompassError, this.options);
+        var compass = navigator.compass.watchHeading(
+            this.onCompassSuccess, 
+            this.onCompassError, 
+            this.options
+        );
+        var geolocation = navigator.geolocation.watchPosition(
+            this.onLocationSuccess, 
+            this.onLocationError, 
+            {maximumAge: 0, timeout: 2000, enableHighAccurracy: true}
+        );
     },
-    onCompassSucess: function(heading){
+    onCompassSuccess: function(heading){
+        document.getElementById('compass').innerHTML = heading.magneticHeading;
         map.setBearing(heading.magneticHeading);
     },
     onCompassError: function(error){
+        console.log(error);
+    },
+    onLocationSuccess: function(position){
+        if(position.coords.accuracy > 65) return;
+        document.getElementById('acc').innerHTML = position.coords.accuracy;
+        marker.setLngLat([position.coords.longitude, position.coords.latitude]);
+        map.setCenter([position.coords.longitude, position.coords.latitude]);
+    },
+    onLocationError: function(error){
         console.log(error);
     }
 };
